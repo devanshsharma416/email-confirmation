a8d264e2441aadb4d2889ea659ff9b225c5da6a4
DEVANSH <devanshsharma416@example.com>
2022-02-04 22:01:26 +0530

    second commit

--- NewApp/utils.py
+++ NewApp/utils.py
@@ -1,7 +1,6 @@
-import imp
 from django.core.mail import EmailMessage
 class Utils:
     @staticmethod
     def send_email(data):
-        email=EmailMessage(subject=data['email_subject'], body = data['email_body'])
+        email=EmailMessage(subject=data['email_subject'], body = data['email_body'], to=[data['to_email']])
         email.send()
